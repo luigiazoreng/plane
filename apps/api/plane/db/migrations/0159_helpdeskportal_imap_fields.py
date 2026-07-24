3cf25b0043f05364823330d5bb4440ce50f600b1
luigiazoreng <luigiazoreng@gmail.com>
2026-07-24 17:48:10 -0300

    fix: correct app name in migration dependencies

--- apps/api/plane/db/migrations/0159_helpdeskportal_imap_fields.py
+++ apps/api/plane/db/migrations/0159_helpdeskportal_imap_fields.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('plane', '0158_helpdesk_form_attachment_field'),
+        ('db', '0158_helpdesk_form_attachment_field'),
     ]
 
     operations = [
